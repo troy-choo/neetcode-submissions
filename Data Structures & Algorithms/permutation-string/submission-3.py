cfab2d2b3453b53216cdba1080f31ba63cb257cf
Troy Choo <troychoo@berkeley.edu>
2026-08-26 18:24:55 +0900

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        dic1 = {}
+        for c in s1:
+            dic1[c] = 1 + dic1.get(c, 0)
+        
+        l = 0
+        dic2 = {}
+
+        for r in range(len(s2)):
+            dic2[s2[r]] = 1 + dic2.get(s2[r], 0)
+
+            if r - l + 1 > len(s1):
+                dic2[s2[l]] -= 1
+
+                if dic2[s2[l]] == 0:
+                    del dic2[s2[l]]
+
+                l += 1
+
+            if dic1 == dic2:
+                return True
+
+        return False
+
