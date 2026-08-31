c54fcb830ac3c70c43ea5842bf4e6fe47e5acd95
Troy Choo <troychoo@berkeley.edu>
2026-08-31 20:07:22 +0900

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            current_val = min(val, self.stack[-1][1])
+            self.stack.append((val, current_val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
